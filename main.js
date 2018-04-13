a2d56669649d6872bfbee15d7ebfc50eb11fb7d2
madacoo <madacoo@protonmail.com>
2018-04-13 15:08:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let BGCOLOR = '#D3E8EE';
 let COLOR = 'rgb(255, 255, 255, 0.2)';
 let WIDTH = 1024;
 let HEIGHT = 768;
+let DENSITY = 10;
 
 function draw() {
     background(BGCOLOR);
@@ -16,11 +17,11 @@ function draw() {
 function setup() {
     canvas(WIDTH, HEIGHT);
     ctx.fillStyle = COLOR;
-    FOUNTAINS.push(new Fountain(WIDTH/2,   HEIGHT,  0, -37, 50));
-    FOUNTAINS.push(new Fountain(150,       HEIGHT,  8, -25, 20));
-    FOUNTAINS.push(new Fountain(WIDTH-150, HEIGHT, -8, -25, 20));
-    FOUNTAINS.push(new Fountain(300,       HEIGHT,  6, -30, 20));
-    FOUNTAINS.push(new Fountain(WIDTH-300, HEIGHT, -6, -30, 20));
+    FOUNTAINS.push(new Fountain(WIDTH/2,   HEIGHT,  0, -37, DENSITY));
+    FOUNTAINS.push(new Fountain(150,       HEIGHT,  8, -25, DENSITY));
+    FOUNTAINS.push(new Fountain(WIDTH-150, HEIGHT, -8, -25, DENSITY));
+    FOUNTAINS.push(new Fountain(300,       HEIGHT,  6, -30, DENSITY));
+    FOUNTAINS.push(new Fountain(WIDTH-300, HEIGHT, -6, -30, DENSITY));
 }
 
 window.onload = function() {
